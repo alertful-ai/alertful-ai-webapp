3527afe87f0031d29fbe748c668accc38d8a7ba1
Pranay Marella <pmarella170@gmail.com>
2024-07-27 16:24:55 -0700

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({
@@ -8,9 +9,7 @@ export default function App({
   pageProps: any;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <Component {...pageProps} />
     </ClerkProvider>
   );
